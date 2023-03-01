7481922c45d8e519cb776916ce39efc90a943e2c
solidOxytocin <73777685+solidOxytocin@users.noreply.github.com>
2023-03-01 19:50:04 +0800

    ActionBar March1

--- app/src/main/java/com/example/howareu/activity/MainMenuActivity.java
+++ app/src/main/java/com/example/howareu/activity/MainMenuActivity.java
@@ -4,23 +4,26 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.EditText;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.example.howareu.R;
-import com.example.howareu.databases.dao.UserDao;
 import com.example.howareu.databases.repository.UserRepository;
-import com.example.howareu.model.User;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainMenuActivity extends AppCompatActivity {
     private TextView nameTextView;
     private UserRepository userRep;
     private FirebaseAuth mAuth;
+    private Toolbar toolbar;
 
 
     @SuppressLint("WrongViewCast")
@@ -29,6 +32,12 @@ public class MainMenuActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         mAuth = FirebaseAuth.getInstance();
         setContentView(R.layout.activity_main_menu);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setTitle("");
+
         setName();
         ImageView msImageView = findViewById(R.id.moodscale);
 
@@ -92,6 +101,34 @@ public class MainMenuActivity extends AppCompatActivity {
             startActivity(intent);
         });
 
+    }
+    //For main menu lagout
+    @Override
+    public boolean onCreateOptionsMenu(@NonNull Menu menu) {
+        new MenuInflater(this).inflate(R.menu.toolbar_main_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        FirebaseAuth auth= FirebaseAuth.getInstance();
+        int id = item.getItemId();
+        switch (id){
+            case R.id.setting:
+                Toast.makeText(this, "Still no Setting Activity", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.about_us:
+                Toast.makeText(this, "Still no About Us Activity", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.lagout:
+                auth.signOut();
+                startActivity(new Intent(this, LoginActivity.class));
+                break;
+            default:
+                break;
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     public void setName(){
